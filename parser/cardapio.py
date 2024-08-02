b4e0577a0f5b7a69ab3d0792ee212d57371368fa
4llyk <allyk.galvincio@gmail.com>
2024-08-02 14:22:41 -0300

    parcero 2 (incompleto)

--- parser/cardapio.py
+++ parser/cardapio.py
@@ -1,3 +1,30 @@
+from xml.dom.minidom import parse
+
+dom = parse("cardapio.xml")
+
+# Elemento raiz do XML (cardapio)
+cardapio = dom.documentElement
+
+# Recebe uma lista dos elementos com tag "prato"
+pratos = cardapio.getElementsByTagName('prato')
+
+# Acessa as informações de cada prato
+for prato in pratos:
+    categoria = prato.getAttribute('categoria')
+    elemento_titulo = prato.getElementsByTagName('título')[0]
+    titulo = elemento_titulo.firstChild.nodeValue
+    elemento_autor = prato.getElementsByTagName('autor')[0]
+    origem = elemento_autor.getAttribute('origem')
+    autor = elemento_autor.firstChild.nodeValue
+    elemento_ano = prato.getElementsByTagName('ano')[0]
+    ano = elemento_ano.firstChild.nodeValue
+
+    print("Categoria:", categoria)
+    print("Título:", titulo)
+    print(f'Autor: {autor} ({origem})')
+    print("Ano:", ano)
+    print("---\n")
+
 def mostrar_menu():
     print('-' *50)
     print("Escolha uma refeição:")
