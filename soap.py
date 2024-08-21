a305611da59f60d0b5639bdc2ec0933b2304085b
4llyk <allyk.galvincio@gmail.com>
2024-08-21 17:53:24 -0300

    soap e gitignore

---
+++ soap.py
@@ -0,0 +1,27 @@
+import requests
+
+# URL do serviço SOAP
+url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso"
+
+# XML estruturado
+country = input("digite o código do pais: ")
+funcao = "CountryFlag"
+payload = f"""<?xml version="1.0" encoding="utf-8"?>
+		<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+		<soap:Body>
+			<{funcao} xmlns="http://www.oorsprong.org/websamples.countryinfo">
+			<sCountryISOCode>{country}</sCountryISOCode>
+			</{funcao}>
+		</soap:Body>
+		</soap:Envelope>"""
+# headers
+headers = {
+	'Content-Type': 'text/xml; charset=utf-8'
+}
+# request POST
+response = requests.request("POST", url, headers=headers, data=payload)
+
+
+# imprime a resposta
+print(response.text)
+print(response)
